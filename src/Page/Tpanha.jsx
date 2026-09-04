1e12949cb77eae5f357aeef43ff1abd34b45069d
thea panha <Acer@theapanha.theapanha>
2026-09-04 21:55:30 +0700

    nhanha

---
+++ src/Page/Tpanha.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Tpanha = () => {
+  return (
+    <div>
+      <h1>nha</h1>
+      <h1>nha</h1>
+      <h1>nha</h1>
+      <h1>nha</h1>
+      <h1>nha</h1>
+      <h1>nha</h1>
+      <h1>nha</h1>
+    </div>
+  )
+}
+
+export default Tpanha
